090be513b2e9e24ea79dbb830e125fa48d76fdf4
oguz gül <ogzgl95@outlook.com>
2021-08-17 19:11:39 +0300

    Redesign and getting new request on the card

--- src/components/subComponents/VideoCard.js
+++ src/components/subComponents/VideoCard.js
@@ -1,15 +1,33 @@
 import React from "react";
 import "./VideoCard.css";
+import TextTruncate from "react-text-truncate";
+import { ThumbUpSharp } from "@material-ui/icons";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
 function VideoCard({ movie }) {
   return (
     <div className="videoCard">
-      <img src={`${base_url}${movie.backdrop_path}`} alt="" />
-      <p>This is a film about coding...</p>
-      <h2>Movie Title</h2>
-      <p>Number of likes...</p>
+      <img
+        src={`${base_url}${movie.backdrop_path || movie.poster_path}`}
+        alt="movie poster"
+      />
+
+      <TextTruncate
+        line={1}
+        element="p"
+        truncateText="..."
+        text={movie.overview}
+      />
+
+      {/* <p>{movie.overview}</p> */}
+      <h2>{movie.title || movie.original_name}</h2>
+      <p className="videoCard__stats">
+        {movie.media_type && `${movie.media_type} ‧`}
+        {movie.release_date || movie.first_air_date} ‧
+        <ThumbUpSharp />{" "}
+        {movie.vote_count}
+      </p>
     </div>
   );
 }
